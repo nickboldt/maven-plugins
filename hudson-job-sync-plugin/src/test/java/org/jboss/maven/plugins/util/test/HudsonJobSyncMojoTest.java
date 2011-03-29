53f855d9af8a939daceb32e5c7db8b279f300985
Nick Boldt <nickboldt@gmail.com>
2011-03-29 11:52:36 -0400

    new hudson-job-sync-plugin

---
+++ hudson-job-sync-plugin/src/test/java/org/jboss/maven/plugins/util/test/HudsonJobSyncMojoTest.java
@@ -0,0 +1,77 @@
+package org.jboss.maven.plugins.util.test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Properties;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.codehaus.plexus.util.FileUtils;
+import org.dom4j.Document;
+import org.jboss.maven.plugins.util.HudsonJobSyncMojo;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class HudsonJobSyncMojoTest {
+
+	private static final String separatorLine = "======================================================================";
+
+	private File tempDir;
+	private File hudsonDir;
+
+	HudsonJobSyncMojo sync;
+
+	String excludes;
+
+	// actual JBoss Hudson server: http://hudson.qa.jboss.com/hudson/
+	private static final String hudsonURL = "http://localhost:8080/";
+	// private static Process hudsonServer;
+	private static boolean verbose;
+
+	// "archives, as, birt, bpel, bpmn, cdi, common, deltacloud, esb, examples, flow, freemarker, gwt, hibernatetools, jbpm, jmx, jsf, jst, maven, modeshape, portlet, profiler, runtime, seam, smooks, struts, tptp, usage, vpe, ws";
+
+	@Before
+	public void setUp() throws IOException {
+		tempDir = createTempDir(getClass().getSimpleName());
+		hudsonDir = createTempDir("hudson");
+		sync = new HudsonJobSyncMojo();
+		sync.setHudsonURL(hudsonURL);
+		sync.setUsername("admin");
+		sync.setPassword("none");
+		verbose = sync.getVerbose();
+		if (verbose)
+			System.out.println(separatorLine);
+	}
+
+	@After
+	public void tearDown() throws IOException {
+		FileUtils.deleteDirectory(tempDir);
+		verbose = sync.getVerbose();
+		if (verbose)
+			System.out.println(separatorLine);
+	}
+
+	@Test
+	public void testExecute() throws IOException, MojoExecutionException {
+		sync.execute();
+		Assert.assertTrue(true);
+	}
+
+	public void execute() throws MojoExecutionException, MojoFailureException {
+	}
+
+	public File createTempDir(String prefix) throws IOException {
+		File directory = File.createTempFile(prefix, "");
+		if (directory.delete()) {
+			directory.mkdirs();
+			return directory;
+		} else {
+			throw new IOException("Could not create temp directory at: "
+					+ directory.getAbsolutePath());
+		}
+	}
+
+}
